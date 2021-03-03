0c80b702176d1a039715be529c99830b5e3fb903
Darien Kirst <darienkirst@gmail.com>
2021-03-02 20:29:22 -0500

    ahhhh hopefully the form doesn't break now?

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,18 +1,55 @@
 import React from 'react';
-export default function ContactForm() {
+import { navigate } from 'gatsby-link'
+
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
+export default function Contact() {
+  const [state, setState] = React.useState({})
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
+
   return (
     <section>
       <h2>Contact</h2>
-      <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
+      <form
+        name="contact"
+        method="post"
+        action="#"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        <input type="hidden" name="form-name" value="contact" />
         <div className="fields">
           <div className="field half">
-            <input type="text" name="name" id="name" placeholder="Name" />
+            <input type="text" name="name" id="name" placeholder="Name" onChange={handleChange}/>
           </div>
           <div className="field half">
-            <input type="email" name="email" id="email" placeholder="Email" />
+            <input type="email" name="email" id="email" placeholder="Email" onChange={handleChange}/>
           </div>
           <div className="field">
-            <textarea name="message" id="message" placeholder="Message" />
+            <textarea name="message" id="message" placeholder="Message" onChange={handleChange}/>
           </div>
         </div>
         <ul className="actions">
